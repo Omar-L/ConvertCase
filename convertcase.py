0ecd60e4c4338cae847a965e5eabdb88a2d7c9a2
Omar Lopez <alexis.omar.lopez@gmail.com>
2022-05-11 23:33:51 -0400

    some improvements, but needs more work

--- convertcase.py
+++ convertcase.py
@@ -3,61 +3,65 @@
 import argparse as a
 from itertools import cycle
 
+class ConvertCase:
+    # def __init__(self, conversions):
+    #     self.conversions = conversions
 
-def alternating_case(s):
-    """
-    Given a string, s, returns the string with aLtErNaTiNg cAsE
-    """
-    case_function = cycle([str.lower, str.upper])
-    return ''.join(func(character) for character, func in zip(s, case_function))
+    def uppercase(s):
+        return s.upper()
 
+    def lowercase(s):
+        return s.lower()
 
-def arg_parse():
-    conversions = ['upper', 'lower', 'title', 'sentence', 'sarcastic', 'all']
+    def titlecase(s):
+        return s.title()
 
-    p = a.ArgumentParser(description='Change the case of a word or sentence.\nWrap sentences with single quotes.')
-    p.add_argument('string', type=str,
-                   help='A string to be converted')
-    p.add_argument('--convert', '--con', choices=conversions,
-                   help='Convert the case of the string. Allowed options are: ' + ', '.join(conversions), metavar='')
-    return p.parse_args()
+    def capitalize(s):
+        return s.capitalize()
 
+    def alternating_case(s):
+        """
+        Given a string, s, returns the string with aLtErNaTiNg cAsE
+        """
+        case_function = cycle([str.lower, str.upper])
+        return ''.join(func(character) for character, func in zip(s, case_function))
 
-def main():
-    args = arg_parse()
+    def arg_parse(self):
+        p = a.ArgumentParser(description='Change the case of a word or sentence.\nWrap sentences with single quotes.')
+        p.add_argument('string', type=str,
+                    help='A string to be converted')
+        p.add_argument('--convert', '--con', choices=conversions,
+                    help='Convert the case of the string. Allowed options are: ' + ', '.join(conversions), metavar='')
+        return p.parse_args()
 
-    # Case conversion for the string
-    uppercase = args.string.upper()
-    lowercase = args.string.lower()
-    title = args.string.title()
-    sentence = args.string.capitalize()
-    sarcastic = alternating_case(args.string)
-    # Add conversions to conversion list
-    conversion = [uppercase,
-                  lowercase,
-                  title,
-                  sentence,
-                  sarcastic]
+    def convert(self):
+        args = ConvertCase.arg_parse(conversions)
 
-    # Uppercase
-    if args.convert == 'upper':
-        print(conversion[0])
-    # Lowercase
-    elif args.convert == 'lower':
-        print(conversion[1])
-    # Title
-    elif args.convert == 'title':
-        print(conversion[2])
-    # Sentence
-    elif args.convert == 'sentence':
-        print(conversion[3])
-    # Sarcastic
-    elif args.convert == 'sarcastic':
-        print(conversion[4])
-    # All conversions
-    elif args.convert == 'all':
-        print(*conversion, sep='\n')
+        if 'upper' in args.convert:
+            print(ConvertCase.uppercase(args.string))
 
+        elif 'lower' in args.convert:
+            print(ConvertCase.lowercase(args.string))
+
+        elif 'title' in args.convert:
+            print(ConvertCase.titlecase(args.string))
+
+        elif 'sentence' in args.convert:
+            print(ConvertCase.capitalize(args.string))
+
+        elif 'sarcastic' in args.convert:
+            print(ConvertCase.alternating_case(args.string))
+
+        elif 'all' in args.convert:
+            print(*conversions, sep='\n')
+
+        # # Add conversions to conversion list
+        # conversion = [uppercase,
+        #             lowercase,
+        #             title,
+        #             sentence,
+        #             sarcastic]
 
 if __name__ == "__main__":
-    main()
+    conversions = ['upper', 'lower', 'title', 'sentence', 'sarcastic', 'all']
+    ConvertCase.convert(conversions)
